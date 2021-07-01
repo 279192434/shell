a0f7d90ab48235a5a842b52286b6ab2438309da8
279192434 <81734543+279192434@users.noreply.github.com>
2021-07-01 17:43:54 +0700

    Update x-wrt-install-vps.sh

--- x-wrt-install-vps.sh
+++ x-wrt-install-vps.sh
@@ -6,7 +6,7 @@ BDEV=sda
 #static ip swap eth0 eth1
 #network="117.18.13.159,255.255.255.0,117.18.13.1,8.8.8.8,initscript=dWNpIHNldCBuZXR3b3JrLmxhbi5pZm5hbWU9ZXRoMQp1Y2kgc2V0IG5ldHdvcmsud2FuLmlmbmFtZT1ldGgwCnVjaSBjb21taXQgbmV0d29yawo="
 #dhcp ip
-#network=dhcp
+network=dhcp
 
 vmroot=/tmp/block
 mkdir -p $vmroot
@@ -16,5 +16,6 @@ cp $vmroot/x-wrt.img.gz /tmp/x-wrt.img.gz && {
 	umount $vmroot
 	sync
 	(zcat /tmp/x-wrt.img.gz;
+	 echo open=443,network=$network;
 	) >/dev/$BDEV && reboot
 }
